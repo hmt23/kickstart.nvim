4989952378dc6fb2f2e9839a661cb92e7ad510bf
Hugo Taboada <hugo.taboada@cea.fr>
2026-04-24 18:33:52 +0200

    add uncrustify as conform default

--- init.lua
+++ init.lua
@@ -696,6 +696,8 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        c = { 'uncrustify' },
+        cpp = { 'uncrustify' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
